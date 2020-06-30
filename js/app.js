61f7449eab25c8e475bb23e0fd406dbcc24ef6b6
jelson <jelson20@outlook.com>
2020-06-30 21:40:16 +0100

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function getElement() {
+	let btn = document.querySelector('#btn');
+
+	btn.addEventListener('click', function () {
+		let list = document.querySelector('#list');
+		let text = document.querySelector('#txt');
+		let li = document.createElement('li');
+
+		li.textContent = text.value;
+
+		list.appendChild(li);
+		text.value = '';
+
+		text.focus();
+	});
+}
+
+window.addEventListener('load', getElement);
